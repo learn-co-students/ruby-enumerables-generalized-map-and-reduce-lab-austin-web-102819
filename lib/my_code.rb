211f6440ffb30c0084d808a2e30f9e6354efe830
Mei Wong <fongmeiw@gmail.com>
2019-09-25 14:55:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
-# Your Code Here
+def map(source_array)
+  if block_given?
+    new = []
+    i = 0
+    while i < source_array.length do
+      new.push(yield(source_array[i]))
+      i += 1
+    end
+    return new
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
+def reduce(array, sv = nil)
+  if block_given?
+    if sv 
+      sum = sv
+      i = 0
+    else
+      sum = array[0]
+      i = 1
+   end
+   
+    while i < array.length do
+      sum = yield(sum, array[i])
+      i += 1
+   end
+  sum
+  else
+    puts "Hey! No block was given!"
+  end
+end
